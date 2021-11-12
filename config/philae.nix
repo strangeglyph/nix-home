6db6d1ca807cc5d87c810de650eb2683eca89a3c
glyph <mail@strangegly.ph>
2021-11-12 23:43:17 +0100

    Philae config 1.0

---
+++ config/philae.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./presets/server.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+
+  networking.hostName = "philae";
+  networking.interfaces.ens3.useDHCP = true;
+  
+  nix = {
+    trustedUsers = [ "root" "@wheel" ];
+    nrBuildUsers = 10;
+  };
+
+  users.users = {
+    glyph = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRwfYnEBA8Pdsqui9xxLkk7KYpKjA01YvzHx8Sfe1PW lschuetze@aeolus"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFCfbpXsvpFUdCa6QL9PMloDtbTyqvvxLML7o/7w2Pi glyph@rosetta"
+      ];
+    };
+    fompf = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+    };
+  };
+
+
+  system.stateVersion = "21.05";
+}
