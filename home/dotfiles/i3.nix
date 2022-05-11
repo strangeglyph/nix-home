e3039ef11c9431e23a157b3f1da698d3da6dacc8
lschuetze <lschuetze@mpi-sws.org>
2022-05-11 15:34:22 +0200

    i3: Use @DEFAULT_SINK@ for volume control so it works when docked

--- home/dotfiles/i3.nix
+++ home/dotfiles/i3.nix
@@ -23,9 +23,9 @@ in {
     ];
   };
   keybindings = lib.mkOptionDefault {
-    "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume 0 +5%";
-    "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume 0 -5%";
-    "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute 0 toggle";
+    "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5%";
+    "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5%";
+    "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
     "XF86MonBrightnessUp" = "exec xbacklight -inc 20";
     "XF86MonBrightnessDown" = "exec xbacklight -dec 20";
     "XF86AudioPlay"  = "exec playerctl play";
