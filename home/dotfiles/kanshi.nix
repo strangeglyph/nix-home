65ebe17757b30a42517e6771b332823d249728b0
lschuetze <lschuetze@mpi-sws.org>
2023-08-03 13:38:15 +0200

    kanshi config

--- home/dotfiles/kanshi.nix
+++ home/dotfiles/kanshi.nix
@@ -1,33 +1,28 @@
 { pkgs, lib, ... }:
 
+
+let
+  work-outputs = left: right: {
+    outputs = [
+      { criteria = left;    position = "0,0";       status = "enable"; }
+      { criteria = right;   position = "1920,0";    status = "enable"; }
+      { criteria = "eDP-1"; position = "1920,1080"; status = "enable"; }
+    ];
+
+    exec = [
+      "swaymsg 'workspace 2, move workspace to ${right}'"
+      "swaymsg 'workspace 8, move workspace to ${left}'"
+    ];
+  };
+in
 {
   enable = true;
   profiles = {
-    "work" = {
-      # Two big monitors horizontal, laptop monitor below right
-      outputs = [
-        {
-          criteria = "DP-4";
-          position = "0,0";
-          status = "enable";
-        }
-        {
-          criteria = "DP-5";
-          position = "1920,0";
-          status = "enable";
-        }
-        {
-          criteria = "eDP-1";
-          position = "1920,1080";
-          status = "enable";
-        }
-      ];
-
-      exec = [
-        "workspace 2, move workspace to DP-5"
-        "workspace 8, move workspace to DP-4"
-      ];
-    };
+    "work" = work-outputs "DP-4" "DP-5";
+    # For some reason the external monitors re-register under different names
+    # when undocking and redocking, so we have multiple entries for the work
+    # setup
+    "work2" = work-outputs "DP-6" "DP-7";
 
     "mobile" = {
       outputs = [{
@@ -35,6 +30,11 @@
         position = "0,0";
         status = "enable";
       }];
+
+      exec = [
+        "swaymsg 'workspace 2, move workspace to eDP-1'"
+        "swaymsg 'workspace 8, move workspace to eDP-1'"
+      ];
     };
   };
 }
