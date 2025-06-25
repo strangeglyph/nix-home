2f4326fda560fa9a4b8d440e59f2d8027986b65c
glyph <mail@strangegly.ph>
2025-06-25 17:20:03 +0200

    Remove nixpkgs.config.allowUnfree from hm-config (belongs into nixos proper)

--- home/default.nix
+++ home/default.nix
@@ -103,8 +103,6 @@
     '';
   };
 
-  nixpkgs.config.allowUnfree = true;
-
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
