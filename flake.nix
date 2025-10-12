298318b71b1978834fa15bbc2b8ed9912e17fcc9
glyph <mail@apophenic.net>
2025-10-12 23:02:07 +0200

    global: use lix from nixpkgs instead of rebuilding from src

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-25.05"; };
-    lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #lix = {
+    #  url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -65,7 +65,7 @@
         system = pkgs.system;
       in {
         imports = [
-          lix.nixosModules.default
+          #lix.nixosModules.default
           home-manager.nixosModules.home-manager
           stylix.nixosModules.stylix
           agenix.nixosModules.default
@@ -90,6 +90,8 @@
             agenix-rekey.packages.${system}.default
             colmena.packages.${system}.colmena
           ];
+
+          nix.package = pkgs.lixPackageSets.latest.lix;
         };
       };
 
