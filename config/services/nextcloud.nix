5624aa3f52fb7ca300ba826526faef129b62e84f
glyph <mail@apophenic.net>
2026-02-05 14:10:01 +0100

    nextcloud: mail settings and config warnings

--- config/services/nextcloud.nix
+++ config/services/nextcloud.nix
@@ -13,6 +13,19 @@ in
   options.glyph.nextcloud.enable = mkEnableOption {};
 
   config = mkIf config.glyph.nextcloud.enable {
+
+    sops.secrets."nextcloud/mail/mailbox" = {};
+    sops.secrets."nextcloud/mail/domain" = {};
+    sops.secrets."nextcloud/mail/pass" = {};
+    sops.templates."nextcloud/mail/login".content = "${config.sops.placeholder."nextcloud/mail/mailbox"}@${config.sops.placeholder."nextcloud/mail/domain"}";
+    # https://github.com/NixOS/nixpkgs/issues/487286
+    sops.templates."nextcloud/secrets".content = builtins.toJSON {
+      mail_from_address = config.sops.placeholder."nextcloud/mail/mailbox";
+      mail_domain = config.sops.placeholder."nextcloud/mail/domain";
+      mail_smtpname = "${config.sops.placeholder."nextcloud/mail/mailbox"}@${config.sops.placeholder."nextcloud/mail/domain"}";
+      mail_smtppassword = config.sops.placeholder."nextcloud/mail/pass";
+    };
+
     services.nextcloud = {
       enable = true;
       https = true;
@@ -32,7 +45,25 @@ in
 
       settings = {
         default_phone_region = "de";
+        maintenance_window_start = 2;
+        mail_smtpsecure = "ssl";
+        mail_smtpport = 465;
+        mail_smtphost = "smtp.migadu.com";
+        mail_smtpauth = true;
       };
+
+      phpOptions = {
+        "opcache.interned_strings_buffer" = "32";
+      };
+
+      secrets = {
+        mail_from_address = config.sops.secrets."nextcloud/mail/mailbox".path;
+        mail_domain = config.sops.secrets."nextcloud/mail/domain".path;
+        mail_smtpname = config.sops.templates."nextcloud/mail/login".path;
+        mail_smtppassword = config.sops.secrets."nextcloud/mail/pass".path;
+      };
+
+      secretFile = config.sops.templates."nextcloud/secrets".path;
     };
 
     services.postgresql = {
